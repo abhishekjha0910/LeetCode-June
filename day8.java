1e24929391d86fa265562c84234ecdcfa251aac9
Abhishek Jha <abhishekjha031@gmail.com>
2024-06-08 18:57:59 +0530

    commit

---
+++ day8.java
@@ -0,0 +1,65 @@
+/*
+ problem 523 :- Continuous subarray sum
+ Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+Note that:
+
+A subarray is a contiguous part of the array.
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+Example 2:
+
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+Example 3:
+
+Input: nums = [23,2,6,4,7], k = 13
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 109
+0 <= sum(nums[i]) <= 231 - 1
+1 <= k <= 231 - 1
+ */
+
+
+
+public class day8 {
+    public static boolean checksubarraysum(int nums[] , int k){
+        int currsum = 0;
+        int maxarray = Integer.MIN_VALUE;
+        for(int i = 0 ; i<nums.length ; i++){
+            currsum += nums[i];
+            if(currsum < 0){
+                currsum = 0 ;
+            }
+        }
+        if(k == 0 && currsum == 0){
+            return true;
+        }
+        else if (k !=0 && currsum % k == 0) {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int nums[] = {23,2,4,6,6};
+        int k = 7;
+        System.out.println(checksubarraysum(nums, k));
+    }
+}
