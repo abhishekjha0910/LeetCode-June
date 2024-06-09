453203af0fa5f066bbb9ddce43b3ffee0f918711
Abhishek Jha <abhishekjha031@gmail.com>
2024-06-09 18:07:07 +0530

    commit

---
+++ day9.java
@@ -0,0 +1,49 @@
+/*
+ problem No : = 974 Subarray sum divisible by k 
+
+ Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+2 <= k <= 104
+ */
+
+public class day9 {
+    public static int subarraysDivByK(int[] nums, int k){
+        int count = 0 ;
+        for(int i = 0 ; i<nums.length ; i++){
+            int sum = 0 ;
+            for(int j = i ; j<nums.length ; j++){
+                sum += nums[j];
+                if( sum % k == 0){
+                    count++;
+                }
+            }
+            
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int nums[] = {4,5,0,-2,-3,1};
+        int k = 5;
+        System.out.println(subarraysDivByK(nums, k));
+    }
+}
